c632e3d4ec133508f81ec9efcabce81bc04a5a6e
Dmytro Gobosh <88391310+MisterTigerillo@users.noreply.github.com>
2023-01-23 20:01:50 +0200

    Update array.js

--- 30DaysOfJS/Day5/array.js
+++ 30DaysOfJS/Day5/array.js
@@ -12,8 +12,28 @@ console.log(
   }`
 );
 // 1.5
-const mixedDataTypes = [5.5, {}, null, undefined, 'Good will', [], 66];
+const mixedDataTypes = [5.5, {}, null, undefined, "Good will", []];
 console.log(mixedDataTypes.length);
 
 // 1.6
-const itCompanies  = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+const itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+
+// 1.7
+console.log(itCompanies);
+
+// 1.8
+console.log(itCompanies.length);
+
+// 1.9
+console.log(itCompanies[0], itCompanies, itCompanies[itCompanies.length - 1]);
+
+// 1.10
+console.log(itCompanies.toString());
