b985341da3d9cb67ce330541dcb9bbf63f104d6e
Dmytro Gobosh <88391310+MisterTigerillo@users.noreply.github.com>
2023-01-24 00:36:03 +0200

    Update array.js

--- 30DaysOfJS/Day5/array.js
+++ 30DaysOfJS/Day5/array.js
@@ -33,7 +33,46 @@ console.log(itCompanies);
 console.log(itCompanies.length);
 
 // 1.9
-console.log(itCompanies[0], itCompanies, itCompanies[itCompanies.length - 1]);
+console.log(itCompanies[0], itCompanies[3], itCompanies[itCompanies.length - 1]);
 
 // 1.10
-console.log(itCompanies.toString());
+console.log(itCompanies.join(', '));
+
+// 1.11
+console.log(itCompanies.toString().toUpperCase());
+// 1.12
+const cutCompanies = itCompanies.slice(0,5);
+console.log(`${cutCompanies} and ${itCompanies[itCompanies.length - 1]} are big IT companies.`);
+
+// 1.13
+if(itCompanies.includes('IBM')) {
+  console.log("This array has your company");
+} else {
+ console.log('Your company is not in the array');
+}
+// 1.14
+// console.log(itCompanies.includes('o').itCompanies.sort());
+// console.log(itCompanies[0].includes('o'));
+// console.log(itCompanies.sort((company) => {
+//   if ("o" > 1)
+//       return 1;
+//   if ("o" <= 1)
+//       return -1;
+//   return 0;
+// }));
+// 1.15
+console.log(itCompanies.sort());
+// 1.16
+console.log(itCompanies.reverse());
+// 1.17
+console.log(itCompanies.slice(0,3));
+// 1.18
+const start = itCompanies.length - 3;
+const end = itCompanies.length;
+console.log(itCompanies);
+console.log(itCompanies.slice(start, end));
+// 1.19
+// 1.20
+// 1.21
+// 1.22
+// 1.23
